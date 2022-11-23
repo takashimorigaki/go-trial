191de0a7b83be957ab6b5d8eefbca2e8ceb71baf
takashimorigaki <tks.coffee123@gmail.com>
2022-11-23 18:14:00 +0900

    Hello, world

--- api/main.go
+++ api/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"go-trial/infra/config"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"fmt"
+	// "go-trial/infra/config"
+	// "github.com/gin-gonic/gin"
+	// "net/http"
 )
 
 func main() {
-	db := config.ConnectDatabase()
-	defer db.Close()
+	// db := config.ConnectDatabase()
+	// defer db.Close()
 
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Initial API",
-		})
-	})
-	router.Run(":3000")
+	// router := gin.Default()
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "Initial API",
+	// 	})
+	// })
+	// router.Run(":3000")
+	fmt.Println("hello, world")
 }
